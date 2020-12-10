ca3748666948f2c070e077340d3b5f64cb50b08f
sdew2-0 <sdewcademy@g.ncu.edu.tw>
2020-12-10 16:01:17 +0800

    新增意見回饋模組

---
+++ iWebSite_ComeIndus/Areas/FeedBack/Controllers/FeedBackController.cs
@@ -0,0 +1,161 @@
+﻿using iWebSite_ComeIndus.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc;
+using iWebSite_ComeIndus.Areas.FeedBack.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using iWebSite_ComeIndus.Controllers;
+using System.Data;
+
+namespace iWebSite_ComeIndus.Areas.FeedBack.Controllers
+{
+    [Area(areaName: "FeedBack")]
+    public class FeedBackController : _BaseController
+    {
+        public static List<FeedBackTypeModel> FDTypes = new List<FeedBackTypeModel> {};
+        public IActionResult Index()
+        {
+            return View("NewFeedBack");
+        }
+
+        [HttpGet]
+        public ActionResult NewFeedBack()
+        {
+            //SQL Select all type
+            var sqlStr = string.Format("select FeedbackTypeNo, TypeName from [dbo].[FeedbackType]");
+            var data = _DB_GetData(sqlStr);
+
+            FDTypes = new List<FeedBackTypeModel>();
+            foreach (DataRow row in data.Rows)
+            {
+                FeedBackTypeModel model = new FeedBackTypeModel();
+                model.FeedbackTypeNo = row.ItemArray.GetValue(0).ToString();
+                model.FeedbackTypeName = row.ItemArray.GetValue(1).ToString();
+                FDTypes.Add(model);
+            }
+
+            ViewData["FDTypes"] = FDTypes;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult NewFeedBack(FeedBackModel Model)
+        {
+            //SQL Insert
+            var sqlStr = string.Format(
+                @"INSERT INTO [dbo].[FeedBack](" +
+                    "[FeedbackTypeNo]," +
+                    "[FeedbackUser]," +
+                    "[FeedbackTitle]," +
+                    "[FeedbackContent]," +
+                    "[CreateTime]" +
+                ")VALUES(" +
+                    "{0}," +
+                    "{1}," +
+                    "{2}," +
+                    "{3}," +
+                    "{4}",
+                    SqlVal2(Model.FeedbackTypeNo),
+                    SqlVal2("sda@ff.ovm"),
+                    SqlVal2(Model.Title),
+                    SqlVal2(Model.Content),
+                    "getDate()" + ")"
+                );
+
+            var check = _DB_Execute(sqlStr);
+
+            //新增是否成功
+            if (check == 1)
+            {
+                ViewData["result"] = "success";
+                //return View("NewFeedBack", "Success!!");
+            }
+            else
+            {
+                ViewData["result"] = "fail";
+            }
+            //return View("NewFeedBack", "Fail :(");
+            ViewData["FDTypes"] = FDTypes;
+            return View();
+        }
+
+        public ActionResult ShowFeedBack()
+        {
+            var sqlStr = string.Format(
+                "select FeedbackNo, TypeName, FeedbackTitle, FeedbackContent, FeedbackUser, [dbo].[Feedback].CreateTime " +
+                "from [dbo].[Feedback] " +
+                "inner join [dbo].[FeedbackType] " +
+                "on [dbo].[Feedback].FeedbackTypeNo = [dbo].[FeedbackType].FeedbackTypeNo");
+
+            var data = _DB_GetData(sqlStr);
+
+            return View(data);
+        }
+        /*
+        [HttpGet]
+        public ActionResult NewFeedBack()
+        {
+            //SQL Select all type
+            var sqlStr = string.Format("select FeedbackTypeNo, TypeName from [dbo].[FeedbackType]");
+            var data = _DB_GetData(sqlStr);
+
+            
+            // prepare for dropdownlist
+            List<SelectListItem> feedbackTypes = new List<SelectListItem>();
+            foreach (DataRow row in data.Rows)
+            {  
+                feedbackTypes.Add(new SelectListItem { 
+                    Text = row.ItemArray.GetValue(1).ToString(),
+                });
+            }
+        
+            FeedBackModel Model = new FeedBackModel();
+            Model.TypeName = feedbackTypes;
+                
+            return View(Model);
+        }
+
+        [HttpPost]
+        public string NewFeedBack(NewFeedBack Model)
+        {
+            //SQL Insert
+            var sqlStr = string.Format(
+                @"INSERT INTO [dbo].[FeedBack]
+                 VALUES(" +
+                    "{0}," +
+                    "{1}," +
+                    "{2}," +
+                    "{3}," +
+                    "{4}," +
+                    "{5}",
+                    SqlVal2("006"),
+                    SqlVal2("FD3"),
+                    SqlVal2("sda@ff.ovm"),
+                    SqlVal2(Model.Title),
+                    SqlVal2(Model.Content),
+                    "getDate()"+ ")"
+                );
+
+            var check = _DB_Execute(sqlStr);
+
+            //新增是否成功
+            if (check == 1) 
+            {
+                return "success";
+            }
+            return "fail";
+
+        }
+
+        public ActionResult ShowFeedBack()
+        {
+            var sqlStr = string.Format("select* from [dbo].[Feedback]");
+            var data = _DB_GetData(sqlStr);
+
+            return View(data);
+        }
+        */
+    }
+}
---
+++ iWebSite_ComeIndus/Areas/FeedBack/Models/FeedBackModel.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace iWebSite_ComeIndus.Areas.FeedBack.Models
+{
+    public class FeedBackTypeModel
+    {
+        public string FeedbackTypeNo { get; set; }
+        public string FeedbackTypeName { get; set; }
+    }
+    public class FeedBackModel : FeedBackTypeModel
+    {
+        public string FeedbackNo { get; set; }
+        
+        public string Username { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime CreateTime { get; set; }
+    }
+    /*
+     public class FeedBackModel
+     {
+         //public IEnumerable<SelectListItem> FeedbackTypeNo { get; set; }
+         public IEnumerable<SelectListItem> TypeName { get; set; }
+     }
+
+     public class NewFeedBack
+     {
+         public string FeedbackNo { get; set; }
+         public string FeedbackTypeNo { get; set; }
+         public string FeedbackTypeName { get; set; }
+         public string Username { get; set; }
+         public string Title { get; set; }
+         public string Content { get; set; }
+         public DateTime CreateTime { get; set; }
+
+     }
+    */
+}
