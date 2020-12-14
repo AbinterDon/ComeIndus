4c5b06ad5380871204e4c23c78c5b2637df2e9da
emsnfi <haach111000@gmail.com>
2020-12-14 21:46:28 +0800

    '會員資料編輯頁面'

---
+++ iWebSite_ComeIndus/Controllers/MemberController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using iWebSite_ComeIndus.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace iWebSite_ComeIndus.Controllers
+{
+    public class MemberController : _BaseController
+    {
+        
+
+        public ActionResult ModifyInfor()
+        {
+            return View();
+        }
+
+        /*public ActionResult Save(Member Model)
+        {
+            var sqlStr = string.Format("select Account,Username,Password,MailCheck,PwdChangeCheck from [dbo].[Member] where Account = {0}", SqlVal2(Model.Account));
+
+            //SQL Check
+            var data = _DB_GetData(sqlStr);
+
+
+
+
+            return View(Model);
+
+        }*/
+    }
+}
---
+++ iWebSite_ComeIndus/wwwroot/js/memberInfor.js
@@ -0,0 +1,86 @@
+﻿
+const pass = false;
+
+
+
+/*圖片上傳*/
+function listenUploadFile() {
+    var uploadb = document.getElementById("uploadb");
+    var inputfile = document.getElementById('inputFile');
+
+    uploadb.addEventListener('click', () => {
+
+        inputfile.click()
+
+    })
+
+    inputfile.addEventListener('change', handleFiles)
+
+}
+
+function handleFiles() {
+    var fileList = this.files;
+    showImg(fileList);
+}
+
+function showImg(thisimg) {
+    var file = thisimg[0];
+
+    if (window.FileReader) {
+        var fr = new FileReader();
+        var showimg = document.getElementById('showimg');
+        fr.onloadend = (e) => {
+            showimg.src = e.target.result;
+        };
+        fr.readAsDataURL(file);
+        var upp = document.getElementById('inputFile');
+        upp.style.opacity = 0;
+        showimg.style.display = 'block';
+    }
+
+}
+
+
+
+
+function checkName() {
+    var userName = document.getElementById('Actualname');
+    var nameCheck = document.getElementById('ActualnameCheck');
+    userName.addEventListener('change', () => {
+        var name = userName.value;
+        if (name == '') {
+            nameCheck.textContent = '你沒有名字? 請輸入你的名字'
+        }
+        else {
+            nameCheck.textContent = '';
+        }
+    })
+
+
+}
+
+function checkPassword() {
+    var password = document.getElementById("Password"); /*第一次輸入密碼*/
+    var passCheck = document.getElementById("PasswordCheck"); /*第二次輸入密碼*/
+    var passMessage = document.getElementById("passCheck"); /*不同時的警訊*/
+    passCheck.addEventListener('change', () => {
+
+        var pwdval = password.value;
+        var pwdCheckValue = passCheck.value;
+
+        if (pwdval != pwdCheckValue) {
+            passMessage.textContent = "密碼輸入不一樣";
+        }
+        else {
+            passMessage.textContent = "";
+        }
+    })
+
+}
+
+
+
+listenUploadFile();
+checkName();
+checkPassword();
+
