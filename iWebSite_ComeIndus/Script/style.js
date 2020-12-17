ba362c147d09586e94421578eb22fe98c4cbd2fe
bettylin0908 <73775591+bettylin0908@users.noreply.github.com>
2020-12-18 00:05:00 +0800

    navbar調整 更新js css layout

--- iWebSite_ComeIndus/Script/style.js
+++ iWebSite_ComeIndus/Script/style.js
@@ -1,10 +1,10 @@
 //滑動離開頂部時就取消at_top的class
-$(window).scroll(function(e){
-  if ($(window).scrollTop()<=0)
-    $(".navbar,.explore").addClass("at_top");
+//$(window).scroll(function(e){
+  //if ($(window).scrollTop()<=0)
+    //$(".navbar,.explore").addClass("at_top");
   else
-    $(".navbar,.explore").removeClass("at_top");
-});
+    //$(".navbar,.explore").removeClass("at_top");
+//});
 
 //緩慢滑動
 $(document).on('click', 'a', function(event){
--- iWebSite_ComeIndus/wwwroot/js/style.js
+++ iWebSite_ComeIndus/wwwroot/js/style.js
@@ -1,9 +1,9 @@
 //滑動離開頂部時就取消at_top的class
-$(window).scroll(function(e){
-  if ($(window).scrollTop()<=0)
-    $(".navbar,.explore").addClass("at_top");
-  else
-    $(".navbar,.explore").removeClass("at_top");
+//$(window).scroll(function(e){
+  //if ($(window).scrollTop()<=0)
+    //$(".navbar,.explore").addClass("at_top");
+  //else
+    //$(".navbar,.explore").removeClass("at_top");
 });
 
 //緩慢滑動
