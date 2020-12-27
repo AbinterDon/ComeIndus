f192a062d2d933ca94fb8dd7589df541a0792728
bettylin0908 <73775591+bettylin0908@users.noreply.github.com>
2020-12-28 01:46:42 +0800

    關於我們更新

---
+++ iWebSite_ComeIndus/Areas/About/Controllers/AboutController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using iWebSite_ComeIndus.Controllers;
+
+namespace iWebSite_ComeIndus.Areas.Aboout.Controllers
+{
+    [Area(areaName: "About")]
+    public class AboutController : _BaseController
+    {
+
+        public IActionResult Index()
+        {
+            return View("About");
+        }
+
+    }
+}
